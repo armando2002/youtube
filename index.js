4961ee4627e09854bb9d6b3b14dff4c93e0d2f52
Stephen Williams <armando2002@gmail.com>
2018-04-28 12:36:08 -0700

    added comments to test code

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const API_KEY = 'AIzaSyAhE7phirGx9wlS2auDO9cIcG_s0NY8ra8';
 // YouTube Search URL
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+
+// testing functionality of API call
+
+// on document load, query API for cats
+$( document ).ready(function() {
+    var testFunction = function(arg1, arg2){
+        console.log(arg1);
+    }
+    $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            q: 'cats',
+            part: 'snippet',
+            key: API_KEY
+        },
+        success: testFunction,
+      });
+});
+
 // AJAX Function
 // 1st try
 // event listener for form input
-$("form").on("submit", function(e) {
+/* $("form").on("submit", function(e) {
     e.preventDefault();
     // debug event listener success
     console.log("input received");
@@ -28,6 +47,8 @@ $.ajax(settings);
 
 // add function to console.log JSON result
 });
+
+*/
 
 
 
